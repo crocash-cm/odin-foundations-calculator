2f17a91ef2d8e81347c5edba1d24ec4b27d65fff
crocash-cm <jashford.work@gmail.com>
2025-07-14 16:00:44 +0100

    Script.js outline.

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstOperand, secondOperand = 0;
+let operator = "";
+
+function add() {
+    return 1;
+}
+
+function subtract() {
+    return 1;
+}
+
+function multiply() {
+    return 1;
+}
+
+function divide() {
+    return 1;
+}
+
+function operate(operator, firstOperand, secondOperand) {
+    return 1;
+}
